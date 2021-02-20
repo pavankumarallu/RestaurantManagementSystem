2d9237ff25c1e5afad0ba97ead80b747686dc62e
pavankumarallu <vamsipavankumarallu@gmail.com>
2021-02-20 14:55:29 +0530

    Added Register Page-2

--- RestaurantManagementSystem/src/Login_Pages/Admin_Customer_Login.java
+++ RestaurantManagementSystem/src/Login_Pages/Admin_Customer_Login.java
@@ -1,19 +1,65 @@
 package Login_Pages;
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+
 public class Admin_Customer_Login extends JFrame {
 
-	private JPanel contentPane;
-
 	/**
-	 * Launch the application.
+	 *
 	 */
-	public static void main(String[] args) {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLayeredPane layeredPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private JTextField username_field;
+	private JPasswordField passwordField_1;
+
+	
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -26,16 +72,204 @@ public class Admin_Customer_Login extends JFrame {
 		});
 	}
 
+	public void switchPanels(JPanel jPanel)
+	{
+		layeredPane.removeAll();
+		layeredPane.add(jPanel);
+		layeredPane.repaint();
+		layeredPane.revalidate();
+		
+	}
 	/**
 	 * Create the frame.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public Admin_Customer_Login() {
+	public Admin_Customer_Login() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		
+		UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
+		JButton Customer_btn;
+		JButton Admin_btn;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1064, 686);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.ORANGE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, 443, 659);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_2 = new JLabel("Alpha");
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setFont(new Font("Tempus Sans ITC", Font.BOLD, 60));
+		lblNewLabel_2.setBounds(128, 303, 189, 103);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Restaurant");
+		lblNewLabel_3.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 49));
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBounds(110, 383, 254, 88);
+		panel.add(lblNewLabel_3);
+		
+		layeredPane = new JLayeredPane();
+		layeredPane.setBounds(444, 99, 616, 560);
+		contentPane.add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel Customer = new JPanel();
+		layeredPane.add(Customer, "name_28724057870000");
+		Customer.setBackground(Color.ORANGE);
+		Customer.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("CUSTOMER");
+		lblNewLabel.setFont(new Font("Tempus Sans ITC", Font.BOLD, 42));
+		lblNewLabel.setBounds(207, 54, 231, 64);
+		Customer.add(lblNewLabel);
+		
+		JLabel lblNewLabel_6 = new JLabel("Username");
+		lblNewLabel_6.setFont(new Font("Tempus Sans ITC", Font.BOLD, 29));
+		lblNewLabel_6.setBounds(79, 181, 161, 42);
+		Customer.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("Password");
+		lblNewLabel_7.setFont(new Font("Tempus Sans ITC", Font.BOLD, 29));
+		lblNewLabel_7.setBounds(79, 267, 161, 37);
+		Customer.add(lblNewLabel_7);
+		
+		username_field = new JTextField();
+		username_field.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 18));
+		username_field.setBounds(314, 186, 225, 37);
+		Customer.add(username_field);
+		username_field.setColumns(10);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 18));
+		passwordField_1.setBounds(314, 267, 231, 37);
+		Customer.add(passwordField_1);
+		
+		JButton btnNewButton_1 = new JButton("Login");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+			//TODO
+			}
+		});
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setBackground(Color.BLACK);
+		btnNewButton_1.setFont(new Font("Tempus Sans ITC", Font.BOLD, 32));
+		btnNewButton_1.setBounds(79, 405, 210, 49);
+		Customer.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Register");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+               //TODO
+				try {
+					btnLoginActionPerformed(evt);
+				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+						| UnsupportedLookAndFeelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		btnNewButton_2.setFont(new Font("Tempus Sans ITC", Font.BOLD, 30));
+		btnNewButton_2.setBackground(Color.BLACK);
+		btnNewButton_2.setForeground(new Color(255, 255, 255));
+		btnNewButton_2.setBounds(365, 406, 210, 49);
+		Customer.add(btnNewButton_2);
+		
+		JPanel Admin = new JPanel();
+		layeredPane.add(Admin, "name_28743664187500");
+		Admin.setBackground(Color.ORANGE);
+		Admin.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("ADMIN");
+		lblNewLabel_1.setFont(new Font("Tempus Sans ITC", Font.BOLD, 44));
+		lblNewLabel_1.setBounds(239, 41, 236, 67);
+		Admin.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_4 = new JLabel("Username");
+		lblNewLabel_4.setFont(new Font("Tempus Sans ITC", Font.BOLD, 26));
+		lblNewLabel_4.setBounds(79, 163, 161, 51);
+		Admin.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Password");
+		lblNewLabel_5.setFont(new Font("Tempus Sans ITC", Font.BOLD, 27));
+		lblNewLabel_5.setBounds(79, 245, 161, 67);
+		Admin.add(lblNewLabel_5);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Verdana", Font.PLAIN, 19));
+		textField.setBounds(284, 170, 236, 37);
+		Admin.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Verdana", Font.PLAIN, 20));
+		passwordField.setBounds(284, 263, 236, 37);
+		Admin.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				//TODO
+			}
+		});
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tempus Sans ITC", Font.BOLD, 33));
+		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.setBounds(241, 403, 193, 67);
+		Admin.add(btnNewButton);
+		
+		Customer_btn = new JButton("Customer");
+		Admin_btn = new JButton("Admin");
+		Customer_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				switchPanels(Customer);
+				Customer_btn.setBackground(new Color(255, 255, 255));
+				Admin_btn.setBackground(Color.BLACK);
+				Customer_btn.setForeground(Color.BLACK);
+				Admin_btn.setForeground(new Color(255, 255, 255));
+				
+			}
+		});
+		Customer_btn.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 45));
+		Customer_btn.setForeground(Color.BLACK);
+		Customer_btn.setBackground(new Color(255,255,255));
+		Customer_btn.setBounds(495, 31, 221, 61);
+		contentPane.add(Customer_btn);
+		
+		
+		Admin_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				switchPanels(Admin);
+				Customer_btn.setBackground(Color.BLACK);
+				Admin_btn.setBackground(new Color(255, 255, 255));
+				Customer_btn.setForeground(new Color(255, 255, 255));
+				Admin_btn.setForeground(Color.BLACK);
+				
+				
+			}
+		});
+		Admin_btn.setForeground(Color.WHITE);
+		Admin_btn.setBackground(Color.BLACK);
+		Admin_btn.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 46));
+		Admin_btn.setBounds(799, 31, 221, 61);
+		contentPane.add(Admin_btn);
 	}
+
+	private void btnLoginActionPerformed(final java.awt.event.ActionEvent evt) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    //TODO
+		Register_Page rp = new Register_Page();
+		rp.setVisible(true);
+		this.dispose();
+    }
 
 }
---
+++ RestaurantManagementSystem/src/Login_Pages/Register_Page.java
@@ -0,0 +1,169 @@
+package Login_Pages;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Register_Page extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField Username_field;
+	private JTextField Phone_number_field;
+	private JPasswordField passwordField;
+	private JPasswordField RepasswordField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Register_Page frame = new Register_Page();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
+	 */
+	public Register_Page() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		
+		UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1064, 686);
+		contentPane = new JPanel();
+		contentPane.setForeground(Color.WHITE);
+		contentPane.setBackground(Color.ORANGE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, 428, 649);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Alpha");
+		lblNewLabel.setFont(new Font("Tempus Sans ITC", Font.BOLD, 56));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(116, 329, 193, 69);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Restaurant");
+		lblNewLabel_1.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 50));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(78, 382, 327, 104);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Register");
+		lblNewLabel_2.setFont(new Font("Tempus Sans ITC", Font.BOLD, 50));
+		lblNewLabel_2.setBounds(621, 53, 216, 62);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel userfield_name = new JLabel("Username");
+		userfield_name.setFont(new Font("Tempus Sans ITC", Font.BOLD, 26));
+		userfield_name.setBounds(500, 222, 132, 46);
+		contentPane.add(userfield_name);
+		
+		JLabel Password_name = new JLabel("Password");
+		Password_name.setFont(new Font("Tempus Sans ITC", Font.BOLD, 26));
+		Password_name.setBounds(500, 290, 132, 46);
+		contentPane.add(Password_name);
+		
+		JLabel Password_name_1 = new JLabel("Re-Enter Password");
+		Password_name_1.setFont(new Font("Tempus Sans ITC", Font.BOLD, 26));
+		Password_name_1.setBounds(500, 355, 223, 46);
+		contentPane.add(Password_name_1);
+		
+		JLabel phone_no_filed_name = new JLabel("Phone number");
+		phone_no_filed_name.setFont(new Font("Tempus Sans ITC", Font.BOLD, 26));
+		phone_no_filed_name.setBounds(500, 424, 186, 46);
+		contentPane.add(phone_no_filed_name);
+		
+		Username_field = new JTextField();
+		Username_field.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+		Username_field.setBounds(752, 231, 202, 40);
+		contentPane.add(Username_field);
+		Username_field.setColumns(10);
+		
+		Phone_number_field = new JTextField();
+		Phone_number_field.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+		Phone_number_field.setColumns(10);
+		Phone_number_field.setBounds(752, 424, 202, 40);
+		contentPane.add(Phone_number_field);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(752, 299, 202, 40);
+		contentPane.add(passwordField);
+		
+		RepasswordField_1 = new JPasswordField();
+		RepasswordField_1.setBounds(752, 361, 202, 40);
+		contentPane.add(RepasswordField_1);
+		
+		JButton Reg_btn = new JButton("Register");
+		Reg_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				try {
+					String user = Username_field.getText();
+					String pass = passwordField.getText();
+					String repass = RepasswordField_1.getText();
+					String ph = Phone_number_field.getText();
+					if(pass.equals(repass))
+					{
+						btnLoginActionPerformed(evt,user,pass,ph);
+					}
+					
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		Reg_btn.setBackground(Color.BLACK);
+		Reg_btn.setFont(new Font("Tempus Sans ITC", Font.BOLD, 40));
+		Reg_btn.setForeground(new Color(255, 255, 255));
+		Reg_btn.setBounds(588, 536, 210, 62);
+		contentPane.add(Reg_btn);
+	}
+	
+	private void btnLoginActionPerformed(final java.awt.event.ActionEvent evt, String user, String pass, String ph) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+        
+	//TODO
+    }
+}
